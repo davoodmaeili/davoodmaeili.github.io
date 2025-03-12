bc03110b8b818a5570495a6535a7f50329f072e9
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2025-03-12 09:23:07 +0100

    fix: add expo text

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -25,7 +25,20 @@ function Gallery({title, imageUrl,galleryPhotos}) {
           </a>
         </div>
       </div>
-
+      {/* Introduction section */}
+      <div className="introduction-section">
+        <div className="introduction-container">
+          <div className="introduction-line"></div>
+          <div className="introduction-text">
+            <p>Cette série est composée de photomontages réalisés en deux périodes distinctes : avant mon immigration en France en 2018 et après, durant la pandémie de Covid.</p>
+            <p>Il y a des années, lorsque j'avais 12 ou 13 ans, j'ai traversé une période dont la durée m'échappe aujourd'hui, mais où je faisais des rêves étranges. Peu à peu, ma conscience de ces rêves s'est intensifiée au point que la frontière entre le rêve et la réalité s'est presque effacée. Ces songes étaient un mélange d'histoires racontées durant mon enfance et d'imaginations qui s'y entremêlaient.</p>
+            <p>Les éléments centraux de cette espièglerie enfantine ont pris la forme de poissons et de forêts, d'humidité et d'errance, et finalement de la perte.</p>
+            <p>Le noyau initial de cette série s'est formé en Iran, s'appuyant sur ces récits et légendes. Quelques années plus tard, pendant le confinement, lorsque ces rêves sont étrangement revenus me hanter, j'ai trouvé un prétexte pour compléter cette série.</p>
+            <p>Je ne peux y mettre un point final, car je ne conçois pas de fin au fait de rêver</p>
+          </div>
+          <div className="introduction-line"></div>
+        </div>
+      </div>
       {/* Gallery grid */}
       <div id="gallery-section" className="gallery-photos-grid">
         {galleryPhotos.map(photo => (
