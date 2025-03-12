4109f43b2971445ef06b0c9075a1f5d40e0572da
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2025-03-12 22:18:14 +0100

    feat: add buy popup

---
+++ src/Buy.jsx
@@ -0,0 +1,41 @@
+
+
+function Buy({ isOpen, onClose, image }) {
+  if (!isOpen) return null;
+  const sizes = [
+    { name: "Memento", size: "10x15 cm", price: "€10" },
+    { name: "Keepsake", size: "20x30 cm", price: "€50" },
+    { name: "Heirloom", size: "30x40 cm", price: "€100" },
+    { name: "Masterpiece", size: "40x60 cm", price: "€300" }
+  ];
+
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <button className="close-button" onClick={onClose}>
+          <i className="fa fa-times"></i>
+        </button>
+        
+        <div className="popup-image-container">
+          <img 
+            src={image.thumbnail} 
+            alt={image.title} 
+            className="popup-image"
+          />
+        </div>
+
+        <div className="size-options">
+          {sizes.map((size, index) => (
+            <button key={index} className="size-option" onClick={() => window.open(`https://wa.me/+33766197937/?text=Salut, j’aimerais bien acheter la photo numéro ${index+1}, ${size.size}!`, "_blank")}>
+              <h3>{size.name}</h3>
+              <p>{size.size}</p>
+              <p className="price">{size.price}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Buy; 
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,30 +1,41 @@
-function Gallery({title, imageUrl,galleryPhotos}) {
+function Gallery({ title, imageUrl, galleryPhotos }) {
+  const [isPopupOpen, setIsPopupOpen] = React.useState(true);
   return (
     <div>
+      <Buy
+        isOpen={isPopupOpen}
+        onClose={() => setIsPopupOpen(false)}
+        image={{
+          thumbnail: "assets/la-chute2.jpg",
+          title: "La chute",
+        }}
+      />
       {/* Donation header */}
       <div className="donation-header">
-        <a href="https://revolut.me/davoodv9s6/eur5/websitedonation" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://revolut.me/davoodv9s6/eur5/websitedonation"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           ☕️ Buy me a coffee
         </a>
       </div>
       {/* Hero section */}
       <div className="iran-container">
-        <img 
-          src={imageUrl}
-          alt="Gallery cover image" 
-          className="hero-image"
-        />
-        
+        <img src={imageUrl} alt="Gallery cover image" className="hero-image" />
+
         <div className="content-overlay">
           <h1 className="title small-title">{title}</h1>
-          <a href="#gallery-section" className="view-gallery-btn">VIEW GALLERY</a>
+          <a href="#gallery-section" className="view-gallery-btn">
+            VIEW GALLERY
+          </a>
         </div>
-        
+
         <div className="photographer-info">
           <a href="/" className="photographer-header">
-            <img 
+            <img
               src="assets/davood.jpg"
-              alt="Davood Maeili" 
+              alt="Davood Maeili"
               className="photographer-logo"
             />
             <span className="photographer-name">DAVOOD MAEILI</span>
@@ -36,32 +47,60 @@ function Gallery({title, imageUrl,galleryPhotos}) {
         <div className="introduction-container">
           <div className="introduction-line"></div>
           <div className="introduction-text">
-            <p>Cette série est composée de photomontages réalisés en deux périodes distinctes : avant mon immigration en France en 2018 et après, durant la pandémie de Covid.</p>
-            <p>Il y a des années, lorsque j'avais 12 ou 13 ans, j'ai traversé une période dont la durée m'échappe aujourd'hui, mais où je faisais des rêves étranges. Peu à peu, ma conscience de ces rêves s'est intensifiée au point que la frontière entre le rêve et la réalité s'est presque effacée. Ces songes étaient un mélange d'histoires racontées durant mon enfance et d'imaginations qui s'y entremêlaient.</p>
-            <p>Les éléments centraux de cette espièglerie enfantine ont pris la forme de poissons et de forêts, d'humidité et d'errance, et finalement de la perte.</p>
-            <p>Le noyau initial de cette série s'est formé en Iran, s'appuyant sur ces récits et légendes. Quelques années plus tard, pendant le confinement, lorsque ces rêves sont étrangement revenus me hanter, j'ai trouvé un prétexte pour compléter cette série.</p>
-            <p>Je ne peux y mettre un point final, car je ne conçois pas de fin au fait de rêver</p>
+            <p>
+              Cette série est composée de photomontages réalisés en deux
+              périodes distinctes : avant mon immigration en France en 2018 et
+              après, durant la pandémie de Covid.
+            </p>
+            <p>
+              Il y a des années, lorsque j'avais 12 ou 13 ans, j'ai traversé une
+              période dont la durée m'échappe aujourd'hui, mais où je faisais
+              des rêves étranges. Peu à peu, ma conscience de ces rêves s'est
+              intensifiée au point que la frontière entre le rêve et la réalité
+              s'est presque effacée. Ces songes étaient un mélange d'histoires
+              racontées durant mon enfance et d'imaginations qui s'y
+              entremêlaient.
+            </p>
+            <p>
+              Les éléments centraux de cette espièglerie enfantine ont pris la
+              forme de poissons et de forêts, d'humidité et d'errance, et
+              finalement de la perte.
+            </p>
+            <p>
+              Le noyau initial de cette série s'est formé en Iran, s'appuyant
+              sur ces récits et légendes. Quelques années plus tard, pendant le
+              confinement, lorsque ces rêves sont étrangement revenus me hanter,
+              j'ai trouvé un prétexte pour compléter cette série.
+            </p>
+            <p>
+              Je ne peux y mettre un point final, car je ne conçois pas de fin
+              au fait de rêver
+            </p>
           </div>
           <div className="introduction-line"></div>
         </div>
       </div>
       {/* Gallery grid */}
       <div id="gallery-section" className="gallery-photos-grid">
-        {galleryPhotos.map(photo => (
+        {galleryPhotos.map((photo) => (
           <div key={photo.id} className="gallery-photo-item">
-            <img 
-              src={photo.src} 
+            <img
+              src={photo.src}
               alt={photo.alt}
               className="gallery-photo"
               loading="lazy"
               onClick={(e) => {
-                e.currentTarget.classList.toggle('zoomed');
-                document.body.classList.toggle('has-zoomed-image');
+                e.currentTarget.classList.toggle("zoomed");
+                document.body.classList.toggle("has-zoomed-image");
               }}
             />
-            <a href={`https://wa.me/+33766197937/?text=Hello, I would like to buy photo number ${photo.id} from your gallery`} className="photo-ribbon" target="_blank" rel="noopener noreferrer">
+            <button
+              // href={`https://wa.me/+33766197937/?text=Hello, I would like to buy photo number ${photo.id} from your gallery`}
+              className="photo-ribbon"
+              onClick={() => setIsPopupOpen(true)}
+            >
               <span>Buy</span>
-            </a>
+            </button>
           </div>
         ))}
       </div>
@@ -69,11 +108,20 @@ function Gallery({title, imageUrl,galleryPhotos}) {
       <footer>
         <div className="footer-content">
           <p>© DAVOOD MAEILI</p>
-          <p>Made with <i className="fa fa-heart"></i> by <a href="https://remotion.live" target="_blank" rel="noopener noreferrer">remotion.live</a></p>
+          <p>
+            Made with <i className="fa fa-heart"></i> by{" "}
+            <a
+              href="https://remotion.live"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              remotion.live
+            </a>
+          </p>
         </div>
       </footer>
     </div>
   );
-};
+}
 
 export default Gallery;
